69e65ec6b6fcc7d0f0c75adca832533e9d8a47ad
AlbanVerg <albano.vergara@gmail.com>
2025-06-27 18:00:27 -0300

    cierre de viernes

--- bases de datos_teoric.R
+++ bases de datos_teoric.R
@@ -1,2 +1,13 @@
 
 # Aca voy a armar la parte teorica de bases de datos
+ 
+
+git status # para ver que hay
+
+git pull # para actualizar el repositorio local con los cambios del remoto
+
+git add # para agregar los archivos que quiero subir
+
+git commit -m "mensaje" # para hacer el commit
+
+git push # para subir los cambios al repositorio remoto
